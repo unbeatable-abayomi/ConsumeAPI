dcf56049dbb2331b7b3b2de232a5da30cd61d1ae
abayomiigwubor <igwubor@outlook.com>
2021-03-02 19:29:51 +0100

    first commit

---
+++ ConsumeWebApiDemo/Controllers/EmployeeManagerController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ConsumeWebApiDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsumeWebApiDemo.Controllers
+{
+    public class EmployeeManagerController : Controller
+    {
+        private readonly HttpClient client = null;
+        private string employeeApiUrl = "";
+
+
+        public EmployeeManagerController(HttpClient client,IConfiguration config )
+        {
+            this.client = client;
+            employeeApiUrl = config.GetValue<string>("AppSettings:EmployeesApiUrl");
+        }
+       //public async Task<IActionResult> Index()
+       // {
+       //     HttpResponseMessage response = await client.GetAsync(employeeApiUrl);
+       //     string stringData = await response.Content.ReadAsStringAsync();
+
+
+       //     var options = new JsonSerializerOptions
+       //     {
+       //         PropertyNameCaseInsensitive = true
+       //     };
+       //     List<Employee> data = JsonSerializer.Deserialize<List<Employee>>(stringData, options);
+       //     return View(data);
+       // }
+
+        public async Task<IActionResult> Index() 
+        {
+            HttpResponseMessage response = await client.GetAsync(employeeApiUrl);
+            string stringData = await response.Content.ReadAsStringAsync();
+
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            List<Employee> data = JsonSerializer.Deserialize<List<Employee>>(stringData, options);
+            return View(data);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            HttpResponseMessage response = await client.GetAsync($"{employeeApiUrl}/{id}");
+            string stringData = await response.Content.ReadAsStringAsync();
+
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+           Employee data = JsonSerializer.Deserialize<Employee>(stringData, options);
+            return View(data);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Employee model)
+        {
+          if(ModelState.IsValid)
+            {
+                string stringData = JsonSerializer.Serialize(model);
+
+                var contentData = new StringContent(stringData, System.Text.Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(employeeApiUrl, contentData);
+                if (response.IsSuccessStatusCode) {
+
+                    ViewBag.Message = "Employee Inserted Sucessfully";
+
+                }
+                else
+                {
+                    ViewBag.Message = "Error While Calling the API";
+                }
+
+            }
+            return View(model);
+        }
+    }
+}
---
+++ ConsumeWebApiDemo/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ConsumeWebApiDemo.Models;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+
+namespace ConsumeWebApiDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient client = null;
+        private string employeeApiUrl = "";
+
+
+        public HomeController(ILogger<HomeController> logger,HttpClient client, IConfiguration config)
+        {
+            _logger = logger;
+            this.client = client;
+            employeeApiUrl = config.GetValue<string>("AppSettings:EmployeesApiUrl");
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            HttpResponseMessage response = await client.GetAsync(employeeApiUrl);
+            string stringData = await response.Content.ReadAsStringAsync();
+
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            List<Employee> data = JsonSerializer.Deserialize<List<Employee>>(stringData, options);
+            return View(data);
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            HttpResponseMessage response = await client.GetAsync($"{employeeApiUrl}/{id}");
+            string stringData = await response.Content.ReadAsStringAsync();
+
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            Employee data = JsonSerializer.Deserialize<Employee>(stringData, options);
+            return View(data);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
